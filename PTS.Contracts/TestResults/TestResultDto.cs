ba36184a7c009118f07738ad7337715e3b9c2991
Chianskihate <islam.kadyrov.dlschool@gmail.com>
2024-03-18 04:32:36 +0300

    fix

---
+++ PTS.Contracts/TestResults/TestResultDto.cs
@@ -0,0 +1,9 @@
+﻿using PTS.Contracts.Test;
+
+namespace PTS.Contracts.TestResults;
+public class TestResultDto
+{
+    public int Id { get; set; }
+    public TestDto Test { get; set; }
+    public string StudentId { get; set; }
+}
