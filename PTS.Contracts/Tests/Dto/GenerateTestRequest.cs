96f1081a4c1875aac62f2ae98668bea8ef2ceb52
Chianskihate <islam.kadyrov.dlschool@gmail.com>
2024-04-26 12:34:46 +0300

    generator time fix

--- PTS.Contracts/Tests/Dto/GenerateTestRequest.cs
+++ PTS.Contracts/Tests/Dto/GenerateTestRequest.cs
@@ -4,7 +4,7 @@ namespace PTS.Contracts.Tests.Dto;
 
 public class GenerateTestRequest
 {
-    public int? Time { get; set; }
+    public int Time { get; set; }
     public int[] ThemeIds { get; set; } = [];
     public TaskComplexity Complexity { get; set; }
     public int TaskCount { get; set; }
