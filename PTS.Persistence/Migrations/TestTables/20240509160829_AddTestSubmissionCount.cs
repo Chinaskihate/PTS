ceb959badaa52e5f7a1e821c87bab8c645fafae7
Chianskihate <islam.kadyrov.dlschool@gmail.com>
2024-05-09 19:08:43 +0300

    test submission migration

---
+++ PTS.Persistence/Migrations/TestTables/20240509160829_AddTestSubmissionCount.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PTS.Persistence.Migrations.TestTables
+{
+    /// <inheritdoc />
+    public partial class AddTestSubmissionCount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TotalSubmissionCount",
+                table: "Tests",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalSubmissionCount",
+                table: "Tests");
+        }
+    }
+}
